2a9447c09157c2a48466d49d3cc39c718c186473
nedsen <65210849+nedsen@users.noreply.github.com>
2024-03-13 13:49:50 +1000

    Add nutrition information for chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -45,10 +48,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-# New section to display fruityvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
-
 
